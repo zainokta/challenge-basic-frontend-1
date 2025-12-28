69153ff61e1b0ba915c2205215d81a1380960e91
Gusti Zainokta <zainokta@gmail.com>
2025-12-28 18:00:46 +0700

    feat: tugas 5 complete

---
+++ tugas_5/script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const nav = document.querySelector('nav');
+    const navToggle = document.querySelector('.mobile-nav-toggle');
+
+    navToggle.addEventListener('click', () => {
+        const visibility = nav.getAttribute('data-visible');
+        
+        if (visibility === "false") {
+            nav.setAttribute('data-visible', true);
+            navToggle.setAttribute('aria-expanded', true);
+        } else {
+            nav.setAttribute('data-visible', false);
+            navToggle.setAttribute('aria-expanded', false);
+        }
+    });
+});
